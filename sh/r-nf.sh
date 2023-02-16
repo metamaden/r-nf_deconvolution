455b5fba66c34521222e74e8a029b3c9044183b0
Sean Maden <metamaden@users.noreply.github.com>
2023-02-16 10:22:58 -0800

    Update r-nf.sh

--- sh/r-nf.sh
+++ sh/r-nf.sh
@@ -2,25 +2,20 @@
 
 # Author: Sean Maden
 #
-# Run a nextflowr workflow.
-#
+# Run an `r-nf` workflow.
 #
 
-# params
+# manage script paths
 rscript_dir=rscript
 write_param_script=r-nf_write-params.R
 gather_script=r-nf_gather-results.R
 
-# update param.config
+# do r-nf run
 echo "updating params.config..."
 Rscript ./$rscript_dir/$write_param_script
-
-# run main workflow
 echo "running workflow..."
 nextflow run main.nf
-
-# gather results into table
 echo "gathering results table..."
 Rscript ./$rscript_dir/$gather_script
 
-echo "finished nextflowr workflow"
+echo "finished r-nf run."
